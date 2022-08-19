cfc5936638ecbdcc9aff1a37304218dacc2007d0
Sergey Shcheglov <97shcheglov@gmail.com>
2022-08-19 12:45:47 +0300

    first commit

---
+++ PlayingWGoogleMaps/PlayingWGoogleMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingWGoogleMapsApp.swift
+//  PlayingWGoogleMaps
+//
+//  Created by Sergey Shcheglov on 19.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PlayingWGoogleMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
